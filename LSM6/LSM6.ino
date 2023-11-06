40b7a40c3c80a6c9ca2d7eb41fea3d80ed8689bd
MoosieySroosiey <kl1timch@gmail.com>
2023-11-06 14:55:14 +0100

    Proper mapping for gyroscope and accelerometer

--- LSM6/LSM6.ino
+++ LSM6/LSM6.ino
@@ -3,11 +3,16 @@
 
 LSM6 imu;
 
-const int bufferSize = 18; 
+const int bufferSize = 10;
 float bufferX[bufferSize];
 float bufferY[bufferSize];
 float bufferZ[bufferSize];
+float bufferGX[bufferSize];
+float bufferGY[bufferSize];
+float bufferGZ[bufferSize];
 int index = 0;
+
+const bool debug = false;
 
 void setup() {
   Serial.begin(9600);
@@ -24,48 +29,62 @@ void setup() {
     bufferX[i] = 0.0;
     bufferY[i] = 0.0;
     bufferZ[i] = 0.0;
+    bufferGX[i] = 0.0;
+    bufferGY[i] = 0.0;
+    bufferGZ[i] = 0.0;
   }
 }
 
 void loop() {
   imu.read();
 
+  bufferX[index] = mapFloat(imu.a.x, -32768, 32767, -2.0, 2.0);
+  bufferY[index] = mapFloat(imu.a.y, -32768, 32767, -2.0, 2.0);
+  bufferZ[index] = mapFloat(imu.a.z, -32768, 32767, -2.0, 2.0);
+  bufferGX[index] = mapFloat(imu.g.x, -32768, 32767, -245.0, 245.0);
+  bufferGY[index] = mapFloat(imu.g.y, -32768, 32767, -245.0, 245.0);
+  bufferGZ[index] = mapFloat(imu.g.z, -32768, 32767, -245.0, 245.0);
 
-  float mappedValueX = mapFloat(imu.a.x, -2000, 2000, -1.0, 1.0);
-  float mappedValueY = mapFloat(imu.a.y, -2000, 2000, -1.0, 1.0);
-  float mappedValueZ = mapFloat(imu.a.z, -2000, 2000, -1.0, 1.0);
+  index = (index + 1) % bufferSize; 
 
-  
-  bufferX[index] = mappedValueX;
-  bufferY[index] = mappedValueY;
-  bufferZ[index] = mappedValueZ;
-
-  float avgX = 0.0;
-  float avgY = 0.0;
-  float avgZ = 0.0;
-
+  float avgX = 0, avgY = 0, avgZ = 0, avgGX = 0, avgGY = 0, avgGZ = 0;
   for (int i = 0; i < bufferSize; i++) {
     avgX += bufferX[i];
     avgY += bufferY[i];
     avgZ += bufferZ[i];
+    avgGX += bufferGX[i];
+    avgGY += bufferGY[i];
+    avgGZ += bufferGZ[i];
   }
-
   avgX /= bufferSize;
   avgY /= bufferSize;
   avgZ /= bufferSize;
+  avgGX /= bufferSize;
+  avgGY /= bufferSize;
+  avgGZ /= bufferSize;
 
-  index = (index + 1) % bufferSize;  
+  Serial.print(avgX, 2);
+  Serial.print("\t");
+  Serial.print(avgY, 2);
+  Serial.print("\t");
+  Serial.print(avgZ, 2);
+  Serial.print("\t");
+  Serial.print(avgGX, 2);
+  Serial.print("\t");
+  Serial.print(avgGY, 2);
+  Serial.print("\t");
+  Serial.println(avgGZ, 2);
 
-
-  Serial.print(avgX, 4); 
- Serial.print("\t");  
- Serial.print(avgY, 4); 
- Serial.print("\t");  
- Serial.println(avgZ, 4);  
+  if (debug) {
+    Serial.print("Raw aX: "); Serial.print(imu.a.x); Serial.print(" Mapped: "); Serial.println(bufferX[index], 2);
+    // Repeat for other values
+  }
 
   delay(10);
 }
 
-float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+float mapFloat(long x, long in_min, long in_max, float out_min, float out_max) {
+  float result = (x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
+  result = constrain(result, out_min, out_max);
+  return result;
 }
--- MIDI.py
+++ MIDI.py
@@ -3,44 +3,55 @@ import serial
 import sys
 import time
 
+def map_to_midi(value, in_min, in_max):
+    # Ensure the value is within the input range
+    value = max(in_min, min(value, in_max))
+    # Map the value to the MIDI range
+    return int((value - in_min) * 127 / (in_max - in_min))
+
 if len(sys.argv) != 2:
     print("Usage: python serial_to_midi.py <serial_port>")
     sys.exit(1)
 
 serial_port = sys.argv[1]
+baud_rate = 9600
+midi_port_name = 'LoopMIDIPort1 1'
 
 mido.set_backend('mido.backends.rtmidi')
-
 print(mido.get_output_names())
 
-midi_port_name = 'port_name'
-
-ser = serial.Serial(serial_port, 9600)
-
 try:
-    with mido.open_output(midi_port_name) as outport:
+    with serial.Serial(serial_port, baud_rate, timeout=1) as ser, mido.open_output(midi_port_name) as outport:
+        print(f"Connected to {serial_port} at {baud_rate} baud.")
         while True:
-            if ser.in_waiting:
-                line = ser.readline().decode('utf-8').strip()  
-                try:
-                    avgX, avgY, avgZ = [float(x) for x in line.split('\t')]  
-                    cc_val_x = int(63.5 + avgX * 63.5)
-                    cc_val_y = int(63.5 + avgY * 63.5)
-                    cc_val_z = int(63.5 + avgZ * 63.5)
+            raw_line = ser.readline()
+            try:
+                line = raw_line.decode('utf-8').rstrip()
+                values = [float(x) for x in line.split('\t')]
+                if len(values) == 6:
+                    cc_val_ax = map_to_midi(values[0], -2.0, 2.0)
+                    cc_val_ay = map_to_midi(values[1], -2.0, 2.0)
+                    cc_val_az = map_to_midi(values[2], -2.0, 2.0)
+                    cc_val_gx = map_to_midi(values[3], -245.0, 245.0)
+                    cc_val_gy = map_to_midi(values[4], -245.0, 245.0)
+                    cc_val_gz = map_to_midi(values[5], -245.0, 245.0)
 
-                    outport.send(mido.Message('control_change', control=1, value=cc_val_x))
-                    outport.send(mido.Message('control_change', control=2, value=cc_val_y))
-                    outport.send(mido.Message('control_change', control=3, value=cc_val_z))
-                except ValueError:
-                
-                    print("Received non-numeric data")
-                time.sleep(0.01)  #Delay to prevent flooding the MIDI port
-
+                    outport.send(mido.Message('control_change', control=1, value=cc_val_ax))
+                    outport.send(mido.Message('control_change', control=2, value=cc_val_ay))
+                    outport.send(mido.Message('control_change', control=3, value=cc_val_az))
+                    outport.send(mido.Message('control_change', control=4, value=cc_val_gx))
+                    outport.send(mido.Message('control_change', control=5, value=cc_val_gy))
+                    outport.send(mido.Message('control_change', control=6, value=cc_val_gz))
+            except UnicodeDecodeError:
+                print(f"Decode error for line: {raw_line.hex()}")
+            except ValueError:
+                print("Received non-numeric data")
+            except IndexError:
+                print("Received incomplete data")
+            time.sleep(0.01)
 except KeyboardInterrupt:
-    print("MIDI port closed.")
-except serial.SerialException as e:
-    print(f"Serial error occurred: {e}")
+    print("Exiting due to KeyboardInterrupt")
 except Exception as e:
     print(f"An error occurred: {e}")
 finally:
-    ser.close()
+    print("Script ended. Ports closed if they were open.")
